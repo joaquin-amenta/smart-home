b89324386fb1fe057ed482fd29c3d84b0bd5abd5
Joaquin Amenta <joaquin.amenta@gmail.com>
2020-07-28 20:40:22 -0300

    commit inicial
    funcionamiento estable de electron(windows) y cordova (android)

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v  </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Links
+        </q-item-label>
+        <q-item clickable exact to="/">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Inicio</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="column items-center">
+      <div class="col">
+        <q-btn
+          size="22px"
+          class="q-px-xl q-py-xl"
+          color="deep-orange"
+          label="Click Me"
+          round
+          @click="aumentar"
+        />
+      </div>
+      <div class="col">
+        <h3>Clickeaste el boton {{cantidad}} veces</h3>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cantidad: 0
+    }
+  },
+  methods: {
+    aumentar() {
+      this.cantidad ++
+    }
+  }
+}
+</script>
